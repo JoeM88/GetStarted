4e2127cda0dc3a8c6152cf3f6cb0641c7589807f
Joseph Molina <jomolina@csumb.edu>
2017-05-03 20:58:23 -0700

    Initial commit

---
+++ app/src/main/java/com/example/josephmolina/getstarted/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.josephmolina.getstarted;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TimePicker timePicker;
+    private TextView alarmTime;
+    private Calendar calendar;
+    private String format = "";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Switch simpleSwitch = (Switch) findViewById(R.id.AlarmSwitch);
+        simpleSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    simpleSwitch.setText(simpleSwitch.getTextOn());
+                }
+
+                if(!isChecked){
+                    simpleSwitch.setText(simpleSwitch.getTextOff());
+                }
+            }
+        });
+
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        alarmTime = (TextView)findViewById(R.id.savedAlarmText);
+        calendar = Calendar.getInstance();
+
+    }
+
+    public void setTime(View view){
+        if(Build.VERSION.SDK_INT >= 23){
+            int hour  = timePicker.getHour();
+            int min = timePicker.getMinute();
+            showTime(hour,min);
+
+        }else{
+            int hour = timePicker.getCurrentHour();
+            int min = timePicker.getCurrentMinute();
+            showTime(hour,min);
+        }
+    }
+
+    private void showTime(int hour, int min) {
+        if(hour == 0){
+            hour += 12;
+            format = "AM";
+        }else if(hour == 12){
+            format = "PM";
+        }else if(hour > 12){
+            hour -= 12;
+            format = "PM";
+        }else{
+            format = "AM";
+        }
+        alarmTime.setText(new StringBuilder().append(hour).append(":").append(min)
+                .append(" ").append(format));
+    }
+}
